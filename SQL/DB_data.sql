c367ca838a7688d7faead1149c8389dad65bb2ce
pawel1719 <42479956+pawel1719@users.noreply.github.com>
2020-03-08 20:30:35 +0100

    Delete DB_data.sql

--- SQL/DB_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `permission`
-    (`ID`, `Name`, `KeyPermission`, `Lang`)
-VALUES
-    (1, 'Administrators', '{"admin": 1}', 'EN' ),
-    (2, 'Advanced user', '{"advanced": 1}', 'EN' ),
-    (3, 'User', '{"user": 1}', 'EN' )
