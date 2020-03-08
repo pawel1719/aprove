f88fddb213bc80dd86b6e122812ce7299b1df1fb
pawel1719 <pawel.szostkiewicz@wp.pl>
2020-03-08 21:49:03 +0100

    Added insert and update to function DB

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 require_once 'core/init.php';
 
 $db = DBB::getInstance();
+//$result = $db->insert('logs', array(
+//    'IPAddress' => '192.168.0.1',
+//    'CreatedAt' => date("Y-m-d H:i:s"),
+//    'Action' => 'Log in - test',
+//    'Devices' => 'Samsung S10'
+//));
+
+$result = $db->update('connestions', '5', array(
+    'IPAddress' => '10.10.10.124',
+    'CreatedAt' => date("Y-m-d H:i:s"),
+    'LastAttempts' => date("Y-m-d H:i:s")
+));
+
+echo $result;
 //$db->query('SELECT * FROM permission');
-$db->get('permission', array('id', '>', '0'));
-
-foreach($db->results() as $user) {
-    echo $user->ID . ' ' . $user->Name . '<br/>';
-}
-
-echo '<hr/>';
-
-echo $db->firstResult()->Name;
+//$db->get('permission', array('id', '>', '0'));
+//
+//foreach($db->results() as $user) {
+//    echo $user->ID . ' ' . $user->Name . '<br/>';
+//}
+//
+//echo '<hr/>';
+//
+//echo $db->firstResult()->Name;
